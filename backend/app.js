3649e071f9ba4ce3eb86f8cf1254bd3844cede1d
Dhanesh Kammath <dhaneshkammath@gmail.com>
2023-10-16 19:35:10 +0530

    cors added to the server

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 });
 
 //using cors
+app.use(cors(corsoption));
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
