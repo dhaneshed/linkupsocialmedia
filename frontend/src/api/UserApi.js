a38f61c0e1a83e1ec4695f22755bcc71a6b8c491
Dhanesh Kammath <dhaneshkammath@gmail.com>
2023-10-24 21:07:14 +0530

    first build 1001

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.REACT_APP_BASE_URL+'/api/v1';
+const baseURL = process.env.REACT_APP_BASE_URL+'/v1';
 export const UserApi=axios.create({
   baseURL,
   withCredentials: true
