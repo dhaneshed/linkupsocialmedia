9faa24d0d0e1d61f6fe433f137f1a4eb886c7140
Dhanesh Kammath <dhaneshkammath@gmail.com>
2024-02-13 17:09:31 +0530

    build 2.0

--- frontend/src/api/AdminApi.js
+++ frontend/src/api/AdminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:4000/api/v1/admin';
+const baseURL = 'https://link-up-mppk.onrender.com/api/v1/admin';
 
 export const AdminApi=axios.create({
   baseURL,
