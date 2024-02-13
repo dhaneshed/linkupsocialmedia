452580af303e7bfd6e1b9b8b0636256850ee29c8
Dhanesh Kammath <dhaneshkammath@gmail.com>
2024-02-13 18:58:48 +0530

    build 6.0

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 exports.connectDatabase = () => {
   mongoose
     .connect(`mongodb://127.0.0.1:27017/SocialMedia`)
+    this.connectDatabase()
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
