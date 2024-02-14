94a0cc9c0a97700344c6111f4ac932e03e97e191
Dhanesh Kammath <dhaneshkammath@gmail.com>
2024-02-14 12:54:59 +0530

    build 9.0

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,20 @@
-// const mongoose = require("mongoose");
-
-// exports.connectDatabase = () => {
-//   mongoose
-//     .connect(`mongodb://127.0.0.1:27017/SocialMedia`)
-//     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
-//     .catch((err) => console.log(err));
-// };
-
 const mongoose = require("mongoose");
 
-exports.connectDatabase = async () => {
-  try {
-    const con = await mongoose.connect("mongodb://127.0.0.1:27017/SocialMedia");
-    console.log(`Database Connected: ${con.connection.host}`);
-  } catch (err) {
-    console.error(err);
-  }
+exports.connectDatabase = () => {
+  mongoose
+    .connect(`mongodb://127.0.0.1:27017/SocialMedia`)
+    .then((con) => console.log(`Database Connected: ${con.connection.host}`))
+    .catch((err) => console.log(err));
 };
+
+// const mongoose = require("mongoose");
+
+// exports.connectDatabase = async () => {
+//   try {
+//     const con = await mongoose.connect("mongodb://127.0.0.1:27017/SocialMedia");
+//     console.log(`Database Connected: ${con.connection.host}`);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
