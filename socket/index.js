89738b2716e3e56c49f2cc0e9e97301ccc3154fe
Dhanesh Kammath <dhaneshkammath@gmail.com>
2024-02-22 10:32:42 +0530

    build 20.0

--- socket/index.js
+++ socket/index.js
@@ -74,4 +74,8 @@ io.on("connection", (socket) => {
     removeUser(socket.id);
   });
 });
-httpsServer.listen(8080);
+
+const PORT = process.env.PORT || 8080; // Use the environment port provided by Render or default to 8080
+httpsServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
